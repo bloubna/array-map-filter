d09de41e32f90d89bc89b66bab688cb634c67c3a
Loubna Bouam <bouamloubna5@gmail.com>
2018-11-10 21:22:26 +0100

    solution filter/ex1

--- filter/ex1.js
+++ filter/ex1.js
@@ -12,11 +12,13 @@ Sortie attendue:
  */
 
 function getPositiveNumbers(numbers) {
+  const results = numbers.filter(Number => Number>=0);
+  return results;
 }
 
 // Si besoin, tu peux toujours visualiser le résultat avec console.log,
 // mais alors, il faut alors lancer l'exercice avec node filter/ex1
-// console.log(getPositiveNumbers([-2, -1, 0, 1, 2]));
+console.log(getPositiveNumbers([-2, -1, 0, 1, 2]));
 
 // Ne pas modifier l'export
 module.exports = getPositiveNumbers;
