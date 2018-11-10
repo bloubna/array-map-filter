57c00327f737fbea3420b3d18c14c93c95f38661
Loubna Bouam <bouamloubna5@gmail.com>
2018-11-10 21:29:37 +0100

    solution filter/ex2

--- filter/ex2.js
+++ filter/ex2.js
@@ -16,7 +16,14 @@ Exemple d'entrée:
  */
 
 function getMultiplesOf(numbers, n) {
+  const results = numbers.filter(number => {
+      if(number % n == 0){
+        return number;
+      }
+  });
+  return results;
 }
 
+console.log(getMultiplesOf([-12, -10, -7, -1, 5, 8, 11, 15, 20, 27],5));
 // Ne pas modifier l'export
 module.exports = getMultiplesOf;
