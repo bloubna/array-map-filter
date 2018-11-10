207e51cd17b599c9c46cd1afe8914cadd289dad2
Loubna Bouam <bouamloubna5@gmail.com>
2018-11-10 23:09:27 +0100

    solution filter-map/ex2

--- filter-map/ex2.js
+++ filter-map/ex2.js
@@ -25,6 +25,38 @@ Sortie attendue:
 */
 
 function getCampusesTeachingReact(campuses) {
+  const citys = campuses.filter(campus => campus.curriculums.includes('JS/React')).map(campus => campus.city);
+  return citys;
 }
 
+
+console.log(getCampusesTeachingReact([{
+    city: 'Bordeaux',
+    curriculums: ['PHP/Symfony', 'JS/React']
+  },
+  {
+    city: 'La Loupe',
+    curriculums: ['JS/Angular']
+  },
+  {
+    city: 'Lille',
+    curriculums: ['PHP/Symfony', 'JS/React']
+  },
+  {
+    city: 'Marseille',
+    curriculums: ['JS/React']
+  },
+  {
+    city: 'Orléans',
+    curriculums: ['PHP/Symfony']
+  },
+  {
+    city: 'Reims',
+    curriculums: ['JS/React']
+  },
+  {
+    city: 'Toulouse',
+    curriculums: ['JEE/Android', 'JS/React']
+  }
+]));
 module.exports = getCampusesTeachingReact;
